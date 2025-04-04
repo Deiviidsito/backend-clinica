6c24b0564fe6325ad9707c7efbf0817a013e258d
David Alvarez <david.alvarez.barraza@gmail.com>
2025-04-03 23:44:03 -0300

    fix(routes): Actualizacion de api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('register', [AuthController::class, 'register']); // Ruta para registrar un nuevo usuario
+Route::post('login', [AuthController::class, 'login']); // Ruta para iniciar sesión
+
+Route::middleware('auth:api')->get('user', [AuthController::class, 'getUser']);  // Ruta para obtener el usuario autenticado
