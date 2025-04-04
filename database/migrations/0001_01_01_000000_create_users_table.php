e316fcdaeca86de344a8a5b579f324636387ee54
David Alvarez <david.alvarez.barraza@gmail.com>
2025-04-03 23:13:23 -0300

    feat(migraciones): modificacion de  migración de usuarios y creacion de migración de roles

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('lastname');
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+            $table->string('rut');
+            $table->string('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
